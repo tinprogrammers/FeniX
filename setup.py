162de7c3afcab27abda479e9b58d69ee3b12d83d
Tin Programmers <tinprogrammers@gmail.com>
2025-08-07 00:01:55 +0500

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyfenix",
-    version="0.1.1",
+    version="0.1.2",
     author="Azeem Teli",
     author_email="tinprogrammers@gmail.com",
     description="A blazing fast and minimal Python full-stack web framework",
